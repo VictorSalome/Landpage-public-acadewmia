9d3856f864c4ee4ec0464f60d357d65df2d3a4ca
Victor Salome <vsalome41@gmail.com>
2024-10-11 13:36:36 -0300

    Fistcommit

---
+++ src/app/page.tsx
@@ -0,0 +1,18 @@
+import { InitialContent } from "./components/InitialContent";
+import { SecundariContent } from "./components/SegundariContent";
+import { TertiaryContent } from "./components/TertiaryContent";
+
+
+export default function Home() {
+  return (
+    <>
+      <InitialContent />
+      <SecundariContent />
+      <TertiaryContent />
+
+
+    </>
+
+  );
+}
+
